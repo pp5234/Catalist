c71dbd0e9b9c619f7077af350862625a1fa1007e
pp5234 <ppetrovic10221ri@raf.rs>
2025-04-26 16:07:57 +0200

    ui: Implement UI for BreedDetailsScreen

--- app/src/main/java/com/example/catalist/cats/details/BreedsDetailsScreen.kt
+++ app/src/main/java/com/example/catalist/cats/details/BreedsDetailsScreen.kt
@@ -1,2 +1,216 @@
-package com.example.catalist.cats.details
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.*
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
+import com.example.catalist.cats.domain.BreedsDetailsData
+import com.example.catalist.core.TextToChips
 
+
+@OptIn(ExperimentalLayoutApi::class, ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+fun BreedsDetailsScreenContent(breed: BreedsDetailsData = BreedsDetailsData(), onWikipediaClick: () -> Unit = {}) {
+    val scrollState = rememberScrollState()
+    val borderModifier =
+        if (breed.isRare) {
+            Modifier.border(
+                width = 8.dp,
+                color = MaterialTheme.colorScheme.primary,
+                shape = RoundedCornerShape(8.dp)
+            )
+        } else Modifier
+
+    Column(
+        modifier = Modifier
+            .verticalScroll(scrollState)
+            .padding(16.dp)
+            .background(MaterialTheme.colorScheme.background),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        //Heading
+        Text(
+            text = breed.name,
+            style = MaterialTheme.typography.headlineLarge,
+            modifier = Modifier.padding(bottom = 8.dp),
+            textAlign = TextAlign.Center
+        )
+        // Image
+        BreedImage(
+            modifier = Modifier
+                .size(250.dp)
+                .then(borderModifier)
+                .padding(16.dp),
+            url = breed.imageUrl,
+            description = "${breed.name} Image",
+        )
+        //Info
+        BreedInfo(
+            modifier = Modifier
+                .width(250.dp)
+                .padding(bottom = 16.dp, top = 4.dp),
+            breed = breed
+        )
+        HorizontalDivider(modifier = Modifier.padding(vertical = 4.dp))
+
+        // Description
+        Text(
+            text = "Description",
+            style = MaterialTheme.typography.titleMedium,
+            modifier = Modifier.padding(bottom = 4.dp)
+        )
+        Text(
+            text = breed.description,
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        HorizontalDivider(modifier = Modifier.padding(vertical = 4.dp))
+        // Temperament
+        Text(
+            text = "Temperament",
+            style = MaterialTheme.typography.titleMedium,
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+        FlowRow(
+            modifier = Modifier.padding(bottom = 16.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+        ) {
+
+            TextToChips(
+                modifier = Modifier
+                    .padding(2.dp),
+                text = breed.temperament,
+                delim = ','
+            )
+
+        }
+        HorizontalDivider(modifier = Modifier.padding(vertical = 4.dp))
+        //BehavioralTraits
+        BreedBehavioralTraits(
+            textModifier = Modifier.padding(bottom = 8.dp),
+            indicatorModifier = Modifier.width(250.dp),
+            breed = breed
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        // Wiki
+        if (breed.wikipediaUrl != null) {
+            Button(
+                onClick = onWikipediaClick,
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            ) {
+                Text("View on Wikipedia")
+            }
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+    }
+}
+
+@Composable
+private fun BreedInfo(modifier: Modifier, breed: BreedsDetailsData) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        BreedInfoColumn(title = "Origin", value = breed.origin)
+        BreedInfoColumn(title = "Life Span", value = "${breed.lifeSpan} years")
+        BreedInfoColumn(title = "Weight", value = "${breed.weightMetric} kg")
+    }
+}
+
+
+@Composable
+private fun BreedImage(modifier : Modifier = Modifier, url : String?, description: String) {
+
+    if (url != null) {
+        AsyncImage(
+            model = url,
+            contentDescription = description,
+            modifier = modifier,
+            contentScale = ContentScale.Crop
+        )
+    } else {
+        Box(
+            modifier = modifier,
+            contentAlignment = Alignment.Center
+        ) {
+            Text("No Image Available", style = MaterialTheme.typography.bodyMedium)
+        }
+    }
+}
+
+@Composable
+private fun BreedInfoColumn(title: String, value: String, modifier: Modifier = Modifier) {
+    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = title, style = MaterialTheme.typography.labelMedium, fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.height(2.dp))
+        Text(text = value, style = MaterialTheme.typography.bodyMedium)
+    }
+}
+
+@Composable
+fun BreedBehaviorTraitIndicator(
+    label: String,
+    level: Int,
+    maxLevel: Int = 5
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 4.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = label,
+            style = MaterialTheme.typography.bodyMedium,
+            modifier = Modifier
+                .weight(1f)
+                .padding(end = 8.dp)
+        )
+
+        LinearProgressIndicator(
+            progress = { level / maxLevel.toFloat() },
+            modifier = Modifier.weight(1f)
+        )
+    }
+}
+
+@Composable
+private fun BreedBehavioralTraits(
+    breed: BreedsDetailsData,
+    textModifier : Modifier = Modifier,
+    indicatorModifier : Modifier = Modifier
+) {
+    Text(
+        text = "Behavioral Traits",
+        style = MaterialTheme.typography.titleMedium,
+        modifier = textModifier
+    )
+    Column(modifier = indicatorModifier) {
+        BreedBehaviorTraitIndicator(label = "Adaptability", level = breed.adaptability)
+        BreedBehaviorTraitIndicator(label = "Intelligence", level = breed.intelligence)
+        BreedBehaviorTraitIndicator(label = "Vocalisation", level = breed.vocalisation)
+        BreedBehaviorTraitIndicator(label = "Health Issues", level = breed.healthIssues)
+        BreedBehaviorTraitIndicator(label = "Grooming Needs", level = breed.grooming)
+        BreedBehaviorTraitIndicator(label = "Social Needs", level = breed.socialNeeds)
+        BreedBehaviorTraitIndicator(label = "Affection Level", level = breed.affectionLevel)
+        BreedBehaviorTraitIndicator(label = "Energy Level", level = breed.energyLevel)
+        BreedBehaviorTraitIndicator(label = "Shedding Level", level = breed.sheddingLevel)
+        BreedBehaviorTraitIndicator(label = "Child Friendly", level = breed.childFriendly)
+        BreedBehaviorTraitIndicator(label = "Dog Friendly", level = breed.dogFriendly)
+        BreedBehaviorTraitIndicator(label = "Stranger Friendly", level = breed.strangerFriendly)
+    }
+}
