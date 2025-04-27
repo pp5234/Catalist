54237dc6746cbabaaeb286a33b8293fb42537184
pp5234 <ppetrovic10221ri@raf.rs>
2025-04-27 17:00:05 +0200

    fix: Align NoDataContent text at center

--- app/src/main/java/com/example/catalist/core/NoDataContent.kt
+++ app/src/main/java/com/example/catalist/core/NoDataContent.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -24,6 +25,7 @@ fun NoDataContent(
         Text(
             text = text,
             fontSize = 24.sp,
+            textAlign = TextAlign.Center
         )
     }
 }
